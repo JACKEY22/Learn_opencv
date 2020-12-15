a0a912c604e4671170647800f4b3e5a2ff9d2edf
jakey22 <sunjeon7@gmail.com>
2020-12-15 19:10:08 +0900

    12/15

--- opencv30.py
+++ opencv30.py
@@ -12,6 +12,7 @@ if not os.path.exists(dir_name):
 
 cap = cv.VideoCapture(0)
 
+count = 0
 while cap.isOpened():
     ret, frame = cap.read()
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -19,20 +20,19 @@ while cap.isOpened():
     
     put_text = "Face not Found!"
     cv.putText(frame, put_text, (50,50), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), 1)
-    cv.imshow("Face Cropper", frame)
+    cv.imshow("Cam", frame_gray)
     if cv.waitKey(10) ==  ord('q'):
         break
 
-    count = 0
     if len(faces) != 0:
         for (x, y, w, h) in faces:
             count = count + 1
-            cropped_area = frame[y:y+h,x:x+w]
+            cropped_area = frame_gray[y:y+h,x:x+w]
             area = cv.resize(cropped_area, (200,200))
         
-
+            
             file_name = dir_name + '/user' + str(count) + '.jpg'
-            cv.imwrite(file_name, cropped_area)
+            cv.imwrite(file_name, area)
 
             put_text = "Face Found!"
             cv.putText(area, put_text, (50,50), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), 1)
